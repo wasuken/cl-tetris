45babd77fa212495f12ba207ebf4e30efde8be34
wasuken <wasuken1@gmail.com>
2020-10-30 22:06:13 +0900

    基礎的な関数はかいた。
    ここからは地獄の並列処理...。

--- README.markdown
+++ README.markdown
@@ -3,3 +3,5 @@
 ## Usage
 
 ## Installation
+
+## Memo
--- src/main.lisp
+++ src/main.lisp
@@ -6,8 +6,9 @@
 (defparameter *map* '())
 (defparameter *width* 10)
 (defparameter *height* 20)
-(defparameter *block* "#")
-(defparameter *blank* "*")
+(defparameter *block* 0)
+(defparameter *blank* 1)
+(defparameter *moving-blocks* 2)
 
 ;;; 初期mapの生成
 (defun gen-map (&optional (node *blank*))
@@ -29,6 +30,14 @@
 							   v)
 							  (t (nth j (nth i map)))))))
 
+(defun pie-convert (block before after)
+  (mapcar #'(lambda (x)
+			  (mapcar #'(lambda (y)
+						  (if (= before y)
+							  after
+							  y))
+					  x))
+		  block))
 
 ;;; utils
 (defun drop-while (f lst)
@@ -37,7 +46,9 @@
 		(t lst)))
 
 (defun map-print (map)
-  (format t "~{~A~%~}" (mapcar #'(lambda (x) (format nil "~{~A~}" x)) map)))
+  (format t "============================~%")
+  (format t "~{~A~%~}" (mapcar #'(lambda (x) (format nil "~{~A~}" x)) map))
+  (format t "============================~%"))
 
 ;; blocks
 ;;; おっぱい
@@ -73,9 +84,8 @@
 (defun put-block (map block &optional (point-x 3))
   (let* ((new-map map)
 		 (manuf-block (drop-while #'(lambda (x)
-									  (every #'(lambda (y) (string= *blank* y))
-											 x)
-									  )
+									  (every #'(lambda (y) (= *blank* y))
+											 x))
 								  block))
 		 (manuf-block-y (length manuf-block))
 		 (manuf-block-x (length (car manuf-block))))
@@ -86,7 +96,58 @@
 					  (replace-map new-map i j (nth (- j point-x) (nth i manuf-block))))))
 	new-map))
 
+(defun check-apl (map node-lst apl)
+  (let ((max-y (length map))
+		(fst (car apl)))
+	(cond ((null apl) t)
+		  ((>= (car fst) max-y) nil)
+		  (t (let* ((eq (find (nth (cdr fst)
+								   (nth (car fst) map))
+							  node-lst)))
+			   ;; (print "********************")
+			   ;; (print apl)
+			   ;; (format t "x: ~A, y: ~A~%" (car fst) (cdr fst))
+			   ;; (print (nth (car fst) (nth (cdr fst) map)))
+			   ;; (print node-lst)
+			   ;; (print "********************")
+			   (cond (eq (check-apl map node-lst (cdr apl)))
+					 (t nil))))))
+  )
+
+(defun update-map (map apl value)
+  (if (null apl)
+	  map
+	  (let ((point (car apl))
+			   (updating-map map))
+		   (setf (nth (cdr point) (nth (car point) updating-map)) value)
+		   (update-map updating-map (cdr apl) value))))
+
 ;;; move
+;;; 全体を動かすかんじで
+(defun move (map &optional (active-point-lst nil))
+  (when (null active-point-lst)
+	(loop for x from 0 to (1- (length map))
+	   do (loop for y from 0 to (1- (length (car map)))
+			 do (if (= (nth y (nth x map)) *moving-blocks*)
+					(setf active-point-lst
+						  (append active-point-lst `((,x . ,y))))))))
+  ;; check
+  ;; 現状はゆーざの操作関係なく勝手に落ちてく状態
+  (let ((moved-apl (mapcar #'(lambda (x) `(,(1+ (car x)) . ,(cdr x)))
+						   active-point-lst)))
+	;; (print "###############################")
+	;; (print (check-apl map `(,*blank* ,*moving-blocks*) moved-apl))
+	;; (print active-point-lst)
+	;; (print "###############################")
+	(cond ((check-apl map `(,*blank* ,*moving-blocks*) moved-apl)
+		   ;; move ok
+		   (let ((moved-map (update-map (update-map map active-point-lst *blank*)
+										moved-apl
+										*moving-blocks*)))
+			 (move moved-map moved-apl)))
+		  ;; move failed
+		  (t (update-map map active-point-lst *block*))))
+  )
 
 ;;; delete
 
@@ -109,8 +170,9 @@
   (map-print *t-pie*)
   (format t "map~%")
   (setf *map* (gen-map))
-  (setf *map* (put-block *map* *t-pie*))
-  (map-print *map*))
+  (setf *map* (put-block *map* (pie-convert *t-pie* *block* *moving-blocks*)))
+  (map-print *map*)
+  (map-print (move *map*)))
 
 ;;; main
 (defun main ()
